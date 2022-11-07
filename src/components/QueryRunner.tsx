62d8f1d033bb0429bfb16bb6b42f9929ff6adb2f
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2022-11-07 09:52:10 +0900

    add hook form undici

--- src/components/QueryRunner.tsx
+++ src/components/QueryRunner.tsx
@@ -2,10 +2,10 @@ import { Textarea, Box, Button, FormControl } from '@chakra-ui/react'
 import { VscRunAll } from 'react-icons/vsc'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import React from 'react'
+import { fetch } from 'undici'
 
 type Form = {
   query: string
-  date: '20221106'
 }
 
 const QueryRunner: React.VFC = () => {
@@ -14,15 +14,36 @@ const QueryRunner: React.VFC = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<Form>({ mode: 'onBlur', reValidateMode: 'onChange' })
+
+  const onSubmit: SubmitHandler<Form> = async (data) => {
+    const res = await fetch('127.0.0.1:3000/query', {
+      body: JSON.stringify({
+        query: data.query,
+        date: '20220101',
+      }),
+      method: 'POST',
+    })
+    console.log('hi')
+  }
   return (
     <>
       <Box marginTop='40px'>
         <Box display='flex' marginBottom='40px'>
-          <Button colorScheme='teal' rightIcon={<VscRunAll />}>
-            実行
-          </Button>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Button colorScheme='teal' rightIcon={<VscRunAll />} type='submit'>
+              実行
+            </Button>
+            <FormControl>
+              <Textarea
+                width='77vw'
+                minH='316px'
+                id='query'
+                placeholder='SELECT * FROM User'
+                {...register('query', { required: '入力してください' })}
+              ></Textarea>
+            </FormControl>
+          </form>
         </Box>
-        <Textarea width='77vw' minH='316px'></Textarea>
       </Box>
     </>
   )
