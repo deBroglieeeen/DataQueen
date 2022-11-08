26f1ccbf520d534b79f83068a25ce1551e856e78
riko <81846197+riko-okubo@users.noreply.github.com>
2022-11-08 22:08:25 +0900

    fix import font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
-// import { Noto_Sans_JP } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import '@fontsource/noto-sans-jp/400.css'
 import '@fontsource/noto-sans-jp/700.css'
 import { theme } from '../styles/theme'
-
-// const notosansJP = Noto_Sans_JP({
-//   weight: '400',
-// })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
