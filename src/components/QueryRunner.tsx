22341ce3d875364a458c63cb55e204c057283726
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2022-11-13 14:08:34 +0900

    remove unused package

--- src/components/QueryRunner.tsx
+++ src/components/QueryRunner.tsx
@@ -1,10 +1,9 @@
-import { Textarea, Box, Button, FormControl } from '@chakra-ui/react'
+import { Box, Button, FormControl } from '@chakra-ui/react'
 import { VscRunAll } from 'react-icons/vsc'
 import { SubmitHandler, useForm } from 'react-hook-form'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
-import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react'
-// import { parseTmTheme } from '@monaco-themes'
+import Editor, { useMonaco } from '@monaco-editor/react'
 
 type Form = {
   query: string
@@ -46,117 +45,12 @@ const QueryRunner: React.VFC<Props> = ({ setResponseData }) => {
     }
   }
 
-  const dataqueen_dark = {
-    base: 'vs',
-    inherit: true,
-    rules: [
-      {
-        background: '454741',
-        token: '',
-      },
-      {
-        foreground: '008e00',
-        token: 'comment',
-      },
-      {
-        foreground: '7d4726',
-        token: 'meta.preprocessor',
-      },
-      {
-        foreground: '7d4726',
-        token: 'keyword.control.import',
-      },
-      {
-        foreground: 'a3be8c',
-        token: 'string',
-      },
-      {
-        foreground: 'b48ead',
-        token: 'constant.numeric',
-      },
-      {
-        foreground: 'c800a4',
-        token: 'constant.language',
-      },
-      {
-        foreground: '275a5e',
-        token: 'constant.character',
-      },
-      {
-        foreground: '275a5e',
-        token: 'constant.other',
-      },
-      {
-        foreground: 'c800a4',
-        token: 'variable.language',
-      },
-      {
-        foreground: 'c800a4',
-        token: 'variable.other',
-      },
-      {
-        foreground: 'c800a4',
-        token: 'keyword',
-      },
-      {
-        foreground: 'c900a4',
-        token: 'storage',
-      },
-      {
-        foreground: '438288',
-        token: 'entity.name.class',
-      },
-      {
-        foreground: '790ead',
-        token: 'entity.name.tag',
-      },
-      {
-        foreground: '450084',
-        token: 'entity.other.attribute-name',
-      },
-      {
-        foreground: '450084',
-        token: 'support.function',
-      },
-      {
-        foreground: '450084',
-        token: 'support.constant',
-      },
-      {
-        foreground: '790ead',
-        token: 'support.type',
-      },
-      {
-        foreground: '790ead',
-        token: 'support.class',
-      },
-      {
-        foreground: '790ead',
-        token: 'support.other.variable',
-      },
-    ],
-    colors: {
-      'editor.foreground': '#000000',
-      'editor.background': '#FFFFFF',
-      'editor.selectionBackground': '#B5D5FF',
-      'editor.lineHighlightBackground': '#00000012',
-      'editorCursor.foreground': '#000000',
-      'editorWhitespace.foreground': '#BFBFBF',
-    },
-  }
-
   const monaco = useMonaco()
   const [editorValue, setEditorValue] = useState('SELECT * FROM User')
   function handleEditorChange(value: any, event: any) {
     console.log('here is the current model value:', value)
     setEditorValue(value)
   }
-  // useEffect(() => {
-  //   if (monaco) {
-  //     console.log('here is the monaco instance:', monaco)
-  //     monaco.editor.defineTheme('dataqueen_dark', dataqueen_dark)
-  //   }
-  // }, [monaco])
 
   return (
     <>
@@ -174,13 +68,6 @@ const QueryRunner: React.VFC<Props> = ({ setResponseData }) => {
                 defaultValue='SELECT * FROM User'
                 onChange={handleEditorChange}
               />
-              {/* <Textarea
-                width='77vw'
-                minH='316px'
-                id='query'
-                placeholder='SELECT * FROM User'
-                {...register('query', { required: '入力してください' })}
-              ></Textarea> */}
             </FormControl>
           </form>
         </Box>
