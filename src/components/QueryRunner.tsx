ba0bc909a5cf0aa4916b960277c361312e5bbbd3
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2022-11-13 14:07:06 +0900

    remove monaco theme package

--- src/components/QueryRunner.tsx
+++ src/components/QueryRunner.tsx
@@ -4,6 +4,7 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react'
+// import { parseTmTheme } from '@monaco-themes'
 
 type Form = {
   query: string
@@ -45,17 +46,117 @@ const QueryRunner: React.VFC<Props> = ({ setResponseData }) => {
     }
   }
 
+  const dataqueen_dark = {
+    base: 'vs',
+    inherit: true,
+    rules: [
+      {
+        background: '454741',
+        token: '',
+      },
+      {
+        foreground: '008e00',
+        token: 'comment',
+      },
+      {
+        foreground: '7d4726',
+        token: 'meta.preprocessor',
+      },
+      {
+        foreground: '7d4726',
+        token: 'keyword.control.import',
+      },
+      {
+        foreground: 'a3be8c',
+        token: 'string',
+      },
+      {
+        foreground: 'b48ead',
+        token: 'constant.numeric',
+      },
+      {
+        foreground: 'c800a4',
+        token: 'constant.language',
+      },
+      {
+        foreground: '275a5e',
+        token: 'constant.character',
+      },
+      {
+        foreground: '275a5e',
+        token: 'constant.other',
+      },
+      {
+        foreground: 'c800a4',
+        token: 'variable.language',
+      },
+      {
+        foreground: 'c800a4',
+        token: 'variable.other',
+      },
+      {
+        foreground: 'c800a4',
+        token: 'keyword',
+      },
+      {
+        foreground: 'c900a4',
+        token: 'storage',
+      },
+      {
+        foreground: '438288',
+        token: 'entity.name.class',
+      },
+      {
+        foreground: '790ead',
+        token: 'entity.name.tag',
+      },
+      {
+        foreground: '450084',
+        token: 'entity.other.attribute-name',
+      },
+      {
+        foreground: '450084',
+        token: 'support.function',
+      },
+      {
+        foreground: '450084',
+        token: 'support.constant',
+      },
+      {
+        foreground: '790ead',
+        token: 'support.type',
+      },
+      {
+        foreground: '790ead',
+        token: 'support.class',
+      },
+      {
+        foreground: '790ead',
+        token: 'support.other.variable',
+      },
+    ],
+    colors: {
+      'editor.foreground': '#000000',
+      'editor.background': '#FFFFFF',
+      'editor.selectionBackground': '#B5D5FF',
+      'editor.lineHighlightBackground': '#00000012',
+      'editorCursor.foreground': '#000000',
+      'editorWhitespace.foreground': '#BFBFBF',
+    },
+  }
+
   const monaco = useMonaco()
   const [editorValue, setEditorValue] = useState('SELECT * FROM User')
   function handleEditorChange(value: any, event: any) {
     console.log('here is the current model value:', value)
     setEditorValue(value)
   }
-  useEffect(() => {
-    if (monaco) {
-      console.log('here is the monaco instance:', monaco)
-    }
-  }, [monaco])
+  // useEffect(() => {
+  //   if (monaco) {
+  //     console.log('here is the monaco instance:', monaco)
+  //     monaco.editor.defineTheme('dataqueen_dark', dataqueen_dark)
+  //   }
+  // }, [monaco])
 
   return (
     <>
