bd94870711df2f7a59db8a911abf5413b9264326
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2022-11-21 15:51:51 +0900

    modified header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
 import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button } from '@chakra-ui/react'
 import React, { useEffect } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import { GetUserByIdQuery, GetUserByIdQueryVariables } from '../generated/graphql'
+import { getUserByIdQuery } from '../graphql/user'
+import { useQuery } from 'urql'
 
 const Header: React.VFC = () => {
   const { user, isAuthenticated, loginWithRedirect } = useAuth0()
+  const [{ data: userResponse }] = useQuery<GetUserByIdQuery, GetUserByIdQueryVariables>({
+    query: getUserByIdQuery,
+    variables: {
+      id: user?.sub ?? '',
+    },
+    pause: user?.sub === undefined,
+  })
   useEffect(() => {
     if (!isAuthenticated) {
       // loginWithRedirect()
@@ -23,7 +33,7 @@ const Header: React.VFC = () => {
           <BreadcrumbLink href='#'>SQLクエリ実行</BreadcrumbLink>
         </BreadcrumbItem>
       </Breadcrumb>
-      <Box>{user ? `(ユーザー:${user.name})` : null}</Box>
+      <Box>{userResponse ? `(ユーザー:${userResponse?.users_by_pk?.name})` : null}</Box>
       <Button onClick={() => loginWithRedirect()}>ログイン</Button>
     </Box>
   )
